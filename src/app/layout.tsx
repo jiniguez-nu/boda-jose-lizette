e0a636fcfc7428bef268695eb298699eee371b6a
Jose M. Iniguez M. <jose.iniguez@nutechnology.com>
2026-04-04 19:49:30 -0600

    replace analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next';
 import { cookies } from 'next/headers';
 import './globals.scss';
-import { Analytics } from '@vercel/analytics/next';
+import { Analytics } from '@vercel/analytics/react';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 import FlowerSystem from '@/components/FlowerSystem';
